b7a17b636427ebb66cd9d3b4d38c3de57a613e66
Christian Katzorke <c.katzorke@web.de>
2012-01-04 22:30:23 +0100

    added couchapp. index.html contains only a single overview (all dvds by mediatype)

---
+++ dvdprofiler/_attachments/script/app.js
@@ -0,0 +1,73 @@
+// Apache 2.0 J Chris Anderson 2011
+$(function() {   
+    // friendly helper http://tinyurl.com/6aow6yn
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+    var path = unescape(document.location.pathname).split('/'),
+        design = path[3],
+        db = $.couch.db(path[1]);
+    function createOverview() {
+        db.view(design + "/media", {
+            group : "true",
+            success : function(data) {
+            	console.log(data);
+                var overview = $.mustache($("#mustache-overview").html(), data);
+                $("#overview").html(overview);
+                $('#carousel-overview').jcarousel({
+                    auto: 2,
+                    wrap: 'last',
+                    visible: 1,
+                    scroll: 1,
+                    animation: "slow",
+                    buttonNextHTML: null,
+                    buttonPrevHTML: null
+                });
+            }
+        });
+    };
+    createOverview();
+    
+    //?
+    var changesRunning = false;
+    function setupChanges(since) {
+        if (!changesRunning) {
+            var changeHandler = db.changes(since);
+            changesRunning = true;
+            changeHandler.onChange(drawItems);
+        }
+    }
+    $("#account").couchLogin({
+    	//TODO
+        loggedIn : function(r) {
+            $("#profile").couchProfile(r, {
+                profileReady : function(profile) {
+                    $("#create-message").submit(function(e){
+                        e.preventDefault();
+                        var form = this, doc = $(form).serializeObject();
+                        doc.created_at = new Date();
+                        doc.profile = profile;
+                        db.saveDoc(doc, {success : function() {form.reset();}});
+                        return false;
+                    }).find("input").focus();
+                }
+            });
+        },
+        loggedOut : function() {
+            $("#profile").html('<p>Please log in to see your profile.</p>');
+        }
+    });
+ });
---
+++ dvdprofiler/views/media/map.js
@@ -0,0 +1,17 @@
+/***
+ * maps all dvds to specific type, i.e. with the reduce function a sum of all dvds of specific type can be created.
+ * Use
+ * http://127.0.0.1:5984/dvdprofiler/_design/dvdprofiler/_view/media
+ * to get a total count {"rows":[{"key":null,"value":1573}]}
+ * but be careful: this does not reflect the number of dvds, since some of them may have more than one media type!
+ * Use
+ * http://127.0.0.1:5984/dvdprofiler/_design/dvdprofiler/_view/media?group=true
+ * to generate a sum for all types {"rows":[{"key":"BluRay","value":361},{"key":"DVD","value":1152},{"key":"HDDVD","value":60}]}
+ */
+function(doc) {
+  if(doc.type == "dvdprofilerentry"){
+	  for(var idx in doc.mediaType){
+		  emit(doc.mediaType[idx].toLowerCase() , 1);
+	  }
+  }
+}
