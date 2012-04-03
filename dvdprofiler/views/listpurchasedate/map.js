e7832af4cc783ecd10691577b83a99aa441effb5
Christian Katzorke <c.katzorke@web.de>
2012-04-04 00:03:06 +0200

    added new fields to view

--- dvdprofiler/views/listpurchasedate/map.js
+++ dvdprofiler/views/listpurchasedate/map.js
@@ -3,6 +3,6 @@
  */
 function(doc) {
  if(doc.type == "dvdprofilerentry"){
-	emit(doc.purchaseDate, {"id": doc._id, "collectionNumber" : doc.collectionNumber, "title": doc.title, "genres": doc.genres, "media": doc.mediaType, "purchaseDate" : doc.purchaseDate, "description": doc.overview, "country": doc.countryOfOrigin});
+	emit(doc.purchaseDate, {"id": doc._id, "collectionNumber" : doc.collectionNumber, "title": doc.title, "genres": doc.genres, "media": doc.mediaType, "purchaseDate" : doc.purchaseDate, "description": doc.overview, "country": doc.countryOfOrigin, "releaseDate": doc.releaseDate, "productionYear": doc.productionYear, "studios": doc.studios, "actors": doc.actors});
  }
 }
